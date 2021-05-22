956a71bd871ad6b615aa9fd29e31c17303531be7
gg <240434@studenti.unimore.it>
2021-05-22 14:52:07 +0200

    i riorganizzazione dei file

---
+++ RFID_basicDetection/RFID_basicDetection.ino
@@ -0,0 +1,48 @@
+/*
+#include <Arduino_FreeRTOS.h>
+
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+#define SS_PIN 53
+#define RST_PIN 49
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init(); //init MFRC522
+  Serial.println("RFID reading UID");
+
+  //Display
+  lcd.init(); //inizializza
+  lcd.backlight(); //accende retroilluminazione
+  lcd.print("RFID reading UID");
+  delay(5000); //---- solo come prova!
+  lcd.clear(); //pulisce display
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if( mfrc522.PICC_IsNewCardPresent() ){
+    if( mfrc522.PICC_ReadCardSerial() ) {
+      Serial.print("Tag UID: ");
+      for(byte i = 0; i < mfrc522.uid.size; i++) {
+        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " ");
+        lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " ");
+        
+        Serial.print(mfrc522.uid.uidByte[i], HEX);
+        lcd.print(mfrc522.uid.uidByte[i], HEX);
+      }
+
+      Serial.println();
+      mfrc522.PICC_HaltA();
+      delay(5000); //TODO: limita due letture consecutive. Serve riorganizzare lettura RFID -> display print
+      lcd.clear();
+    }
+  }
+}
+*/
---
+++ RFID_task/RFID_task.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+#include <Arduino_FreeRTOS.h>
+
+//definizione pin
+#define SS_PIN 53
+#define RST_PIN 49
+
+//definizione task
+void TaskReadRFID( void *pvParameters );
+void TaskDisplay( void *pvParameters );
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup(){
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init(); //init MFRC522
+  Serial.println("RFID reading UID");
+
+  //Display
+  lcd.init(); //inizializza
+  lcd.backlight(); //accende retroilluminazione
+  lcd.print("RFID reading UID");
+  delay(5000); //---- solo come prova!
+  lcd.clear(); //pulisce display
+
+}
+
+void loop() {
+    
+}
