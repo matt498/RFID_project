b3c3083d675fea8dab20509e441bfca4a2790e5f
gg <240434@studenti.unimore.it>
2021-05-26 17:29:38 +0200

    Aggiunta dei task APERTURA e CHIUSURA con bottoni e aggiunta di LED di stato della macchina

--- RFID_task/RFID_task.ino
+++ RFID_task/RFID_task.ino
@@ -3,20 +3,47 @@
 #include <LiquidCrystal_I2C.h>
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
+#include <Button.h>
 
 //definizione pin
 #define SS_PIN 53
 #define RST_PIN 49
+#define closeLED 8
+#define openLED 9
+#define rangeLED 10
 
 //definizione task
 void TaskReadRFID( void *pvParameters );
 void TaskDisplay( void *pvParameters );
+void TaskOpenButton( void *pvParameters );
+void TaskCloseButton( void *pvParameters );
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 SemaphoreHandle_t mutex;
-bool letturaRFID = false;
+bool autorizzazioneCarta = false;
+//********
+//false = carta NON nella portata del lettore RFID
+//true = carta nella portata del lettore RFID
+//bool cardState = false
+
+//Button def
+const int openPin = 2;
+const int closePin = 3;
+
+//codice carta registrata
+byte arrayByteCard[4] = {
+  0xE9,
+  0xBB,
+  0xBB,
+  0xB8
+  };
+
+//buffer TAG CARD
+byte bufferTag[4];
+
+int contatoreCarta = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -30,6 +57,15 @@ void setup(){
   lcd.print("RFID reading UID");
   delay(5000); //---- solo come prova!
   lcd.clear(); //pulisce display
+
+  //Button
+  pinMode(openPin, INPUT);
+  pinMode(closePin, INPUT);
+
+  //LED
+  pinMode(closeLED, OUTPUT);
+  pinMode(openLED, OUTPUT);
+  pinMode(rangeLED, OUTPUT);
 
   mutex = xSemaphoreCreateMutex();
   if (mutex != NULL) {
@@ -52,6 +88,21 @@ void setup(){
   ,  1  // Priority
   ,  NULL );
 
+    xTaskCreate(
+  TaskOpenButton
+  ,  "OpenButton"
+  ,  128  // Stack size
+  ,  NULL
+  ,  1  // Priority
+  ,  NULL );
+
+  xTaskCreate(
+  TaskCloseButton
+  ,  "CloseButton"
+  ,  128  // Stack size
+  ,  NULL
+  ,  1  // Priority
+  ,  NULL );
 
 }
 
@@ -63,14 +114,31 @@ void TaskReadRFID( void *pvParameters ){
     if (xSemaphoreTake(mutex, 10) == pdTRUE){
       if( mfrc522.PICC_IsNewCardPresent() ){
         if( mfrc522.PICC_ReadCardSerial() ) {
-          letturaRFID = true;
           Serial.print("Tag UID: ");
           for(byte i = 0; i < mfrc522.uid.size; i++) {
             Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " ");
-            
             Serial.print(mfrc522.uid.uidByte[i], HEX);
+            bufferTag[i] = mfrc522.uid.uidByte[i];
           }
-      
+          Serial.println();
+          if(arrayByteCard[0] == bufferTag[0] &&
+             arrayByteCard[1] == bufferTag[1] &&
+             arrayByteCard[2] == bufferTag[2] &&
+             arrayByteCard[3] == bufferTag[3] ){
+              contatoreCarta++;
+              if(contatoreCarta % 2 == 1){
+                autorizzazioneCarta = true;
+                Serial.println("CARTA IN PORTATA");
+                digitalWrite(rangeLED, HIGH);
+              }
+              if(contatoreCarta % 2 == 0){
+                autorizzazioneCarta = false;
+                Serial.println("CARTA FUORI PORTATA");
+                contatoreCarta = 0;
+                digitalWrite(rangeLED, LOW);
+              }
+          }
+           
           Serial.println();
           mfrc522.PICC_HaltA();
           delay(1500); //TODO: limita due letture consecutive. Serve riorganizzare lettura RFID -> display print
@@ -86,11 +154,51 @@ void TaskReadRFID( void *pvParameters ){
 void TaskDisplay( void *pvParameters ){
   for(;;){
     if (xSemaphoreTake(mutex, 10) == pdTRUE){
-      if(letturaRFID == true){
-        Serial.println("visualizzazione su schermo OK");
-        letturaRFID = false;
+      if(autorizzazioneCarta == true){
+        for(byte i = 0; i < 4; i++) {
+            lcd.print(bufferTag[i] < 0x10 ? "0" : " ");
+            lcd.print(bufferTag[i], HEX);
+        }
       }
     xSemaphoreGive(mutex);
+    }
+    vTaskDelay(1);
+  }
+}
+
+void TaskOpenButton( void *pvParameters ){
+  for(;;){
+    if (xSemaphoreTake(mutex, 10) == pdTRUE){
+      if(autorizzazioneCarta == true){
+        int buttonState = digitalRead(openPin);
+
+        if (buttonState == HIGH) {
+        // turn LED on:
+          Serial.println("Pressione di APERTURA");
+          digitalWrite(openLED, HIGH);
+          digitalWrite(closeLED, LOW);
+        }
+      }
+      xSemaphoreGive(mutex);
+    }
+    vTaskDelay(1);
+  }
+}
+
+void TaskCloseButton( void *pvParameters ){
+  for(;;){
+    if (xSemaphoreTake(mutex, 10) == pdTRUE){
+      if(autorizzazioneCarta == true){
+        int buttonState = digitalRead(closePin);
+
+        if (buttonState == HIGH) {
+        // turn LED on:
+          Serial.println("Pressione di CHIUSURA");
+          digitalWrite(openLED, LOW);
+          digitalWrite(closeLED, HIGH);   
+        }
+      }
+      xSemaphoreGive(mutex);
     }
     vTaskDelay(1);
   }
